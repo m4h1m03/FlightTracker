8cffc6a82e2b967aeddf2b17c3265ad8e1fbfee5
Mahim Ahmed <187803022+m4h1m03@users.noreply.github.com>
2026-05-06 19:40:21 +0100

    Add observations bulk insert and file cleanup

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -9,21 +9,21 @@ import psycopg
 from dotenv import load_dotenv
 import os
 import requests
-import json
 from datetime import datetime, timezone
 
 load_dotenv()   # reads variables from .env file and sets them in the os.environ
 
-# -- fetch live data from OpenSki --
+# -- fetch live data from OpenSky --
 response = requests.get('https://opensky-network.org/api/states/all')
 response_dict = response.json()
 
-# -- Build aircraft rows for bulk insert
+# -- Build aircraft rows for bulk insert --
 flights = response_dict['states']
 aircraft_rows = []
 for flight in flights:
     aircraft = (flight[0], flight[2])
     aircraft_rows.append(aircraft)
+
 
 with psycopg.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), host= os.getenv('DB_HOST'), port=os.getenv('DB_PORT')) as conn:
     with conn.cursor() as cur:
@@ -35,8 +35,7 @@ with psycopg.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), hos
         fetch_status = 'success'
         cur.execute(
         'INSERT INTO snapshots (snapshot_time, flight_count, fetch_duration, fetch_status) VALUES (%s, %s, %s, %s) RETURNING id',
-        (snapshot_time, flight_count, fetch_duration, fetch_status)
-        )
+        (snapshot_time, flight_count, fetch_duration, fetch_status))
         snapshot_id = cur.fetchone()[0]
         print(snapshot_id)
 
@@ -45,9 +44,32 @@ with psycopg.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_USER'), hos
         'INSERT INTO aircraft (icao24, origin_country) VALUES (%s, %s) ON CONFLICT (icao24) DO NOTHING',
          aircraft_rows
         )
-       
-            
-        # cur.executemany(
-        # 'INSERT INTO observations'
-        # )
+        # -- Build observation rows for bulk insert --
+        observation_rows = []
+        for flight in flights:
+            observation = (snapshot_id, flight[0], None if flight[1] is None else flight[1].strip(), None if flight[3] is None else datetime.fromtimestamp(flight[3], tz=timezone.utc), datetime.fromtimestamp(flight[4], tz=timezone.utc) , flight[5], flight[6], flight[7], flight[8], flight[9], flight[10], flight[11], flight[12], flight[13], flight[14], flight[15], flight[16])
+            observation_rows.append(observation)    
+
+        cur.executemany('''
+        INSERT INTO observations (
+        snapshot_id, 
+        icao24,
+        callsign, 
+        time_position,
+        last_contact,
+        longitude,
+        latitude,
+        baro_altitude,
+        on_ground,
+        velocity,
+        true_track,
+        vertical_rate,
+        sensors,
+        geo_altitude,
+        squawk,
+        spi,
+        position_source
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+        observation_rows                
+        )
 
